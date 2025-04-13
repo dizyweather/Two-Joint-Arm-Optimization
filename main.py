6e587564470aac1f369d2798b00ba8036ed8161a
IsaiahMillington <ismi2675@colorado.edu>
2025-04-13 16:57:31 -0600

    Pushing all changes in local repo

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 
 from Arm import Arm
+from Simulation import Simulation
 import matplotlib.pyplot as plt
 import numpy as np
-
-hi = (0, 0)
-hi = (hi[0] + 1, hi[1])
 
 arm = Arm((0, 0))
 arm.add_linkeage(0, 1)
 arm.add_linkeage(np.pi /2, 1)
-arm.draw(plt)
+
+sim = Simulation((1, 1), arm)
+sim.draw(plt)
 
 plt.show()
 
