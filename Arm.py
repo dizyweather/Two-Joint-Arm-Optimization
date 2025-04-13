3ff6b07c5a3ed6f44fc342cf11ae6024af4cee76
Daniel Zhu <daniel.zhu@colorado.edu>
2025-04-13 16:24:44 -0600

    fixed visulization in main and added calculate end pos function to arm

--- Arm.py
+++ Arm.py
@@ -8,7 +8,7 @@ class Arm:
         pass
 
     # Instead have a function that can add lengths and linkeages to an array
-    linkeage_angles = [] # in radians (floats)
+    linkeage_angles = [] # angle of lineage in radians in global frame (floats)
     linkeage_lengths = [] # in meters or change to like pixels? (floats)
 
     # Adds a new linkeage to the arrays
@@ -18,7 +18,17 @@ class Arm:
 
     # Based on the linkeages' length and angle, return an x, y of the final/end effector's position
     def calculate_end_position(self):
-        pass
+        current_position = self.start_position
+
+        for i in range(len(self.linkeage_angles)):
+            current_length = self.linkeage_lengths[i]
+            current_angle = self.linkeage_angles[i]
+
+            current_position = (current_position[0] + current_length * np.cos(current_angle), 
+                          current_position[1] + current_length * np.sin(current_angle))
+
+        
+        return current_position
 
     def change_angles(self, array_of_angles):
         self.linkeage_angles = array_of_angles
@@ -26,12 +36,15 @@ class Arm:
 
     # given a matplot plot, draw yourself on said plot
     def draw(self, plt):
+        plt.clf() # clear figure
+
         current_position = self.start_position
-        current_position = (0, 0)
 
         for i in range(len(self.linkeage_angles)):
             current_length = self.linkeage_lengths[i]
             current_angle = self.linkeage_angles[i]
+
+            print(current_angle)
 
             next_point = (current_position[0] + current_length * np.cos(current_angle), 
                           current_position[1] + current_length * np.sin(current_angle))
--- main.py
+++ main.py
@@ -9,7 +9,14 @@ hi = (hi[0] + 1, hi[1])
 arm = Arm((0, 0))
 arm.add_linkeage(0, 1)
 arm.add_linkeage(np.pi /2, 1)
+
+arm.change_angles([np.pi, np.pi])
 arm.draw(plt)
+
+plt.xticks(range(-5, 5))
+plt.yticks(range(-1, 5))
 
 plt.show()
 
+
+print(np.sin(np.pi))
