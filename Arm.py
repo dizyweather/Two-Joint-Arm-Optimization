4082bc4313bce514a9caa95632a61105a78a93bf
Daniel Zhu <daniel.zhu@colorado.edu>
2025-04-13 15:46:23 -0600

    inital outline of arm class

---
+++ Arm.py
@@ -0,0 +1,19 @@
+class Arm:
+    # Constructor
+    # Could have an empty constrcutor
+    
+    # Instead have a function that can add lengths and linkeages to an array
+    linkeage_angles = [] # in radians
+    linkeage_lengths = [] # in meters or change to like pixels?
+
+    # Adds a new linkeage to the arrays
+    def add_linkeage(angle, length):
+        pass
+
+    # Based on the linkeages' length and angle, return an x, y of the final/end effector's position
+    def calculate_end_position():
+        pass
+
+    # given a matplot plot, draw yourself on said plot
+    def draw(plot):
+        pass
