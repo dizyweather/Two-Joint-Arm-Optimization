1cacedc012ce31fdf4a62a115fd577ff170ec935
IsaiahMillington <ismi2675@colorado.edu>
2025-04-20 15:51:27 -0600

    fixed test.ipynb adding arm linkeages in each run, also added remove_linkeage function

--- Arm.py
+++ Arm.py
@@ -1,20 +1,26 @@
 import numpy as np
+from Simulation import Simulation
+import matplotlib.pyplot as plt
 
 class Arm:
     # Constructor
     # Could have an empty constrcutor
     def __init__(self, start_position):
         self.start_position = start_position # tuple
+        # Instead have a function that can add lengths and linkeages to an array
+        self.linkeage_angles = [] # angle of lineage in radians in global frame (floats)
+        self.linkeage_lengths = [] # in meters or change to like pixels? (floats)
         pass
-
-    # Instead have a function that can add lengths and linkeages to an array
-    linkeage_angles = [] # angle of lineage in radians in global frame (floats)
-    linkeage_lengths = [] # in meters or change to like pixels? (floats)
 
     # Adds a new linkeage to the arrays
     def add_linkeage(self, angle, length):
         self.linkeage_angles.append(angle)
         self.linkeage_lengths.append(length)
+
+    def remove_linkeage(self):
+        if self.linkeage_angles and self.linkeage_lengths:
+            self.linkeage_angles.pop()
+            self.linkeage_lengths.pop()
 
     # Based on the linkeages' length and angle, return an x, y of the final/end effector's position
     def calculate_end_position(self):
@@ -55,5 +61,19 @@ class Arm:
             plt.plot(x_vals, y_vals)
 
             current_position = next_point
+
+# Outside the arm class, simple test case function
+def run_sim():
+
+    arm = Arm((0, 0))
+    arm.add_linkeage(0, 1)
+    arm.add_linkeage(np.pi / 2, 1)
+
+    sim = Simulation((1, 1), arm)
+    sim.draw(plt)
+    plt.axis('equal')
+    plt.show()
+
+run_sim()
         
         
